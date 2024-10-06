09d46e583993187a43de285c27eda7159ae43e61
felipepatente <felipepatente@yahoo.com.br>
2024-10-05 22:31:38 -0300

    Criando um banco de dados

---
+++ SQL para Desenvolvedores/Criando um banco de dados.sql
@@ -0,0 +1,9 @@
+--CREATE DATABASE DesenvolvedorIO;
+
+--ALTER DATABASE DesenvolvedorIO SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+
+--DROP DATABASE DesenvolvedorIO;
+
+--CREATE DATABASE DesenvolvedorIO 
+--ON (Name = 'Dev_mdf', FILENAME = 'D:\teste.mdf') 
+--LOG ON (Name = 'Dev_log', FILENAME = 'D:\teste.ldf');
