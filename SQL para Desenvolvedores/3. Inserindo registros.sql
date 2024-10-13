01ef642d1b2ff395e7432da5d33777b4fffa56e6
felipepatente <felipepatente@yahoo.com.br>
2024-10-13 11:44:47 -0300

    Inserindo registros

---
+++ SQL para Desenvolvedores/3. Inserindo registros.sql
@@ -0,0 +1,12 @@
+--INSERT INTO Alunos 
+--	(Nome, Cpf, DataNascimento, Ativo)
+--VALUES
+--	('Rafael', '40566100859', GETDATE(), 0)
+
+
+INSERT INTO Alunos 
+	(Nome, Cpf, DataNascimento, Ativo)
+VALUES
+	('Eduardo', '40466100859', GETDATE(), 0),
+	('Bruno', '40766101859', GETDATE(), 0)
+
