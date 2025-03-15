801c66ecdaafd3e04160855654d9b616e3e993e6
felipepatente <felipepatente@yahoo.com.br>
2025-03-14 23:39:50 -0300

    Removendo coluna de uma tabela

---
+++ SQL para Desenvolvedores/33. Removendo coluna de uma tabela.sql
@@ -0,0 +1,3 @@
+ALTER TABLE Categorias DROP CONSTRAINT DF__Categoria__Teste__73BA3083;
+ALTER TABLE Categorias DROP CONSTRAINT DF__Categoria__NovoC__74AE54BC;
+ALTER TABLE Categorias DROP COLUMN Teste, NovoCampo
