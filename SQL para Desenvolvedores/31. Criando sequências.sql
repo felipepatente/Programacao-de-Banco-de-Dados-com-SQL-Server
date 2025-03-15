5022c9ffcf4b2225fd5e274b08988359ac8c211d
felipepatente <felipepatente@yahoo.com.br>
2025-03-14 23:32:21 -0300

    Criando sequências

---
+++ SQL para Desenvolvedores/31. Criando sequências.sql
@@ -0,0 +1,21 @@
+CREATE SEQUENCE MinhaSequencia
+AS INT
+START WITH 6
+INCREMENT BY 1
+MINVALUE 6
+MAXVALUE 1000
+NO CYCLE
+
+DROP SEQUENCE MinhaSequencia
+
+SELECT NEXT VALUE FOR MinhaSequencia
+
+CREATE TABLE TabelaTeste
+(
+	Id INT DEFAULT NEXT VALUE FOR MinhaSequencia,
+	Descricao VARCHAR(20)
+)
+
+INSERT INTO TabelaTeste (Descricao) VALUES ('Teste 02')
+
+SELECT * FROM TabelaTeste (NOLOCK)
